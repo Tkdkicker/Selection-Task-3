3a7dfa4301f65539fb60b686fc853f5fe99af678
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:38:08 +0000

    Add project files.

---
+++ Selection - Task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Selection___Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade");
+            char grade = char.Parse(Console.ReadLine());
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Well done");
+                    break;
+                case 'C':
+                    Console.WriteLine("You passed");
+                    break;
+                case 'D':
+                    Console.WriteLine("Better try again");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+            Console.WriteLine("Your grade is " + grade);
+            Console.ReadLine();
+        }
+    }
+}
